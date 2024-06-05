b9f14203b5d742e97fa0ee98c2b040aa3ef1bb2e
Shivam Barya <58471837+shiv-4s@users.noreply.github.com>
2024-06-05 18:51:27 +0000

    comparison

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);   //  retun true because of type coersion it convert string 2 to Number 2;
+// console.log("02asdf" > 1);  //  return NaN
+
+// console.log(null > 0);  //  comparison operator ( >, <, >=, <=) converts the null value to Intger 0 but equality operator (==) doesn't convert it to 0;
+// console.log(null == 0);
+// console.log(null >= 0);
