fee2ec3007ff7942ab65a6b61f80dc7f2366efda
Shivam Barya <58471837+shiv-4s@users.noreply.github.com>
2024-06-05 18:21:27 +0000

    conversion of datatypes

---
+++ 01_Basics/conversionoperation.js
@@ -0,0 +1,25 @@
+let score = false;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1, false => 0
+
+let isLoggedIn = "shivam";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//  1 => true, 0 => false
+//  "" => false
+//  "shiv" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
