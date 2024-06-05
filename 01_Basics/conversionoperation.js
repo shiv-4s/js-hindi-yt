87e0bb8e76c571a08b11af02333f1a64c56170d7
Shivam Barya <58471837+shiv-4s@users.noreply.github.com>
2024-06-05 18:39:44 +0000

    typerconversion

--- 01_Basics/conversionoperation.js
+++ 01_Basics/conversionoperation.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "shivam";
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //  1 => true, 0 => false
 //  "" => false
@@ -21,5 +21,25 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************OPERATIONS***********************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello ";
+let str2 = "Shivam";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);   //  12
+// console.log(1 + "2");   //  12
+// console.log("1" + 2 + 2);   //  122
+// console.log(1 + 2 + "2");  //   32  
+
+// console.log(+true);  //  return 0
+// console.log(+"");    //  return 0
+// console.log(+"hi");  //  return NaN
