77162dd74a55a7d54c4fe4d0d58bf0eaee9e569d
Shivam Barya <58471837+shiv-4s@users.noreply.github.com>
2024-06-05 18:05:52 +0000

    datataypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer version.
+// alert(3 + 3) // we're using node js not browser
+let name = "Shivam"
+let age = 28;
+let isLoggedIn = false;
+
+
+//  number => 2 to power 53
+//  bigInt
+//  string => ""
+//  boolean => true/false
+//  null => standalone value
+//  undefined =>
+//  symbol => unique
+
+console.log(typeof undefined);
