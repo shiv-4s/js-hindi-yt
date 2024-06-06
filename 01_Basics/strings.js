3154b68fbb3c764579473d07e02bb87d446eb5f8
Shivam Barya <58471837+shiv-4s@users.noreply.github.com>
2024-06-06 17:57:28 +0000

    strings

---
+++ 01_Basics/strings.js
@@ -0,0 +1,24 @@
+const name = "Shivam"
+const repoCount = 10;
+
+// console.log(name + repoCount + " Value");    old skool implementation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String("Shivamrc");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+const newStringOne = "   shivam     ";
+console.log(newStringOne.trim());
