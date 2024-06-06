d4eb55dc9be923401c75b4faa970bf7840dc4a98
Shivam Barya <58471837+shiv-4s@users.noreply.github.com>
2024-06-06 18:30:09 +0000

    maths

---
+++ 01_Basics/nums_and_maths.js
@@ -0,0 +1,27 @@
+const score = 400;
+console.log(score);
+
+const  balance = new  Number(100)
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+const otherNumber = 123.866
+// console.log(otherNumber.toPrecision(2))
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// *************************MATH**********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));  //  return always positive value
+// console.log(Math.round(4.6))    //  return 5
+// console.log(Math.ceil(4.2)) //  return 5
+
+console.log(Math.random())
+console.log((Math.floor(Math.random() * 10)) + 1)
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min)
