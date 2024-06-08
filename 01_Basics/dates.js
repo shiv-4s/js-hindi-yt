b50ed63e516a1bd027a0cfac54df5cfacb538d88
Shivam Barya <58471837+shiv-4s@users.noreply.github.com>
2024-06-08 16:26:00 +0000

    dates

---
+++ 01_Basics/dates.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+
+// let myCreatedDate = new Date("02-02-2024");
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+// console.log(myCreatedDate.getTime());
